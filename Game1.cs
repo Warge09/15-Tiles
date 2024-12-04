1c5c1e4035f20e0edada1dc528e6b01aab7f3fb3
Warge09 <charlie10avfc@icloud.com>
2024-12-04 23:52:29 +0000

    Add files via upload

---
+++ Game1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Collections.Generic;
+using System.Collections;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+
+namespace _15Tiles
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        private const int WINDOW_HEIGHT = 940;
+        private const int WINDOW_WIDTH = 1080;
+
+        private int Moves;
+
+        int[,] _Tiles = new int[4, 4];
+
+
+        private Texture2D BackgroundImage;
+        private Texture2D TileImage;
+        private Texture2D TitleScreen;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            _graphics.PreferredBackBufferWidth = WINDOW_WIDTH;
+            _graphics.PreferredBackBufferHeight = WINDOW_HEIGHT;
+            this.Window.AllowUserResizing = false;
+            this.Window.Title = "15 Tiles Game"; 
+            IsMouseVisible = true;
+
+            Moves = 0;
+
+        }
+
+        protected override void Initialize()
+        {       
+            
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            BackgroundImage = Content.Load<Texture2D>("content/background");
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(BackgroundImage, new Rectangle(0, 0, GraphicsDevice.Viewport.Width, GraphicsDevice.Viewport.Height), Color.White);
+            _spriteBatch.End();
+
+
+            base.Draw(gameTime);
+        }
+    }
+}
